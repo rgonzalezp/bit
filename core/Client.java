f7edf902f405dd868f23ac802c823d1f5a843e73
RICARDO ENRIQUE GONZALEZ PEÐUELA <re.gonzalez10@sis.virtual.uniandes.edu.co>
2018-10-03 19:26:07 -0500

    changes

--- core/Client.java
+++ core/Client.java
@@ -9,10 +9,14 @@ import utils.Datafile;
 import utils.Logger;
 import utils.MessageSender;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.URI;
+import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Set;
@@ -69,7 +73,24 @@ public class Client {
         ConcurrentHashMap<Peer, Float> unchokedPeers = new ConcurrentHashMap<>();
 
         // probably shouldn't be local host if running on zoo or something
-        InetSocketAddress client = new InetSocketAddress(InetAddress.getLocalHost(), port);
+        String systemipaddress = "";
+        try
+        { 
+            URL url_name = new URL("http://bot.whatismyipaddress.com"); 
+  
+            BufferedReader sc = 
+            new BufferedReader(new InputStreamReader(url_name.openStream())); 
+  
+            // reads system IPAddress 
+            systemipaddress = sc.readLine().trim(); 
+        } 
+        catch (Exception e) 
+        { 
+            systemipaddress = "Cannot Execute Properly"; 
+        } 
+       
+        
+        InetSocketAddress client = new InetSocketAddress(systemipaddress, port);
         TrackerClient trackerClient = new TrackerClient(client, metaFile.getAnnounce(), datafile);
 
         TrackerResponse initResponse = getInitialTrackerResponse(trackerClient, registerFile, connections, trackerClient, logger);
