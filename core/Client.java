fae1cbab3ca885be224e7be326b8ff4694b83900
RICARDO ENRIQUE GONZALEZ PEÐUELA <re.gonzalez10@sis.virtual.uniandes.edu.co>
2018-10-03 19:55:28 -0500

    changes

--- core/Client.java
+++ core/Client.java
@@ -14,11 +14,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
 import java.net.Socket;
 import java.net.URI;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.Set;
 import java.util.concurrent.*;
 
@@ -73,24 +76,20 @@ public class Client {
         ConcurrentHashMap<Peer, Float> unchokedPeers = new ConcurrentHashMap<>();
 
         // probably shouldn't be local host if running on zoo or something
-        String systemipaddress = "";
-        try
-        { 
-            URL url_name = new URL("http://bot.whatismyipaddress.com"); 
-  
-            BufferedReader sc = 
-            new BufferedReader(new InputStreamReader(url_name.openStream())); 
-  
-            // reads system IPAddress 
-            systemipaddress = sc.readLine().trim(); 
-        } 
-        catch (Exception e) 
-        { 
-            systemipaddress = "Cannot Execute Properly"; 
-        } 
-       
-        
-        InetSocketAddress client = new InetSocketAddress(systemipaddress, port);
+        InetAddress ip = InetAddress.getLocalHost();
+        Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+        for (NetworkInterface netint : Collections.list(nets))
+        {
+        	if( netint.getName().toString().equals("ens160"))
+            {
+        		 Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
+                 for (InetAddress inetAddress : Collections.list(inetAddresses)) {
+                 	ip = inetAddress;
+                 }
+            }
+             
+        }
+        InetSocketAddress client = new InetSocketAddress(ip, port);
         TrackerClient trackerClient = new TrackerClient(client, metaFile.getAnnounce(), datafile);
 
         TrackerResponse initResponse = getInitialTrackerResponse(trackerClient, registerFile, connections, trackerClient, logger);
@@ -102,7 +101,7 @@ public class Client {
         new Thread(new Welcomer(port, BACKLOG, connections, logger, datafile)).start();
         new Thread(new Responder(connections, unchokedPeers, datafile, executor, logger)).start();
     }
-
+    
     // Get an initial peer list from the tracker and attempt to initiate connections with all peers.
     private static TrackerResponse getInitialTrackerResponse(TrackerClient trackerClient,
                                                              boolean registerFile,
